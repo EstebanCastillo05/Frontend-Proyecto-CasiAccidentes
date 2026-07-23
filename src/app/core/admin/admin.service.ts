1d94d01ca16f6c3e1862d7cfd7b67f76ad9f839c
Emanuel Gonzalez <eg554848@gmail.coml>
2026-07-23 00:30:17 -0600

    birgadas

--- src/app/core/admin/admin.service.ts
+++ src/app/core/admin/admin.service.ts
@@ -2,7 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { API_BASE_URL } from '../api.config';
-import { ApiResponse, CreateUserRequest, Role, UpdateUserRequest, User } from './admin.models';
+import {
+  ApiResponse,
+  Brigada,
+  BrigadaCatalogos,
+  CreateBrigadaRequest,
+  CreateUserRequest,
+  Role,
+  UpdateBrigadaRequest,
+  UpdateUserRequest,
+  User,
+} from './admin.models';
 
 @Injectable({ providedIn: 'root' })
 export class AdminService {
@@ -35,6 +45,36 @@ export class AdminService {
   getRoles(): Observable<Role[]> {
     return this.http
       .get<ApiResponse<Role[]>>(`${API_BASE_URL}/roles`)
+      .pipe(map((response) => response.data));
+  }
+
+  getBrigadas(): Observable<Brigada[]> {
+    return this.http
+      .get<ApiResponse<Brigada[]>>(`${API_BASE_URL}/brigadas`)
+      .pipe(map((response) => response.data));
+  }
+
+  getBrigadaCatalogos(): Observable<BrigadaCatalogos> {
+    return this.http
+      .get<ApiResponse<BrigadaCatalogos>>(`${API_BASE_URL}/brigadas/catalogos`)
+      .pipe(map((response) => response.data));
+  }
+
+  createBrigada(data: CreateBrigadaRequest): Observable<Brigada> {
+    return this.http
+      .post<ApiResponse<Brigada>>(`${API_BASE_URL}/brigadas`, data)
+      .pipe(map((response) => response.data));
+  }
+
+  updateBrigada(id: number, data: UpdateBrigadaRequest): Observable<Brigada> {
+    return this.http
+      .put<ApiResponse<Brigada>>(`${API_BASE_URL}/brigadas/${id}`, data)
+      .pipe(map((response) => response.data));
+  }
+
+  deleteBrigada(id: number): Observable<Brigada> {
+    return this.http
+      .delete<ApiResponse<Brigada>>(`${API_BASE_URL}/brigadas/${id}`)
       .pipe(map((response) => response.data));
   }
 }
